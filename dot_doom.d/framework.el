5ab40e8c9c068ff8301cd4a3a747d66e51528e60
Josh <josh@joshgilliland.com>
2025-07-03 17:12:11 -0500

    Add .doom.d/Josh-Gilliland-MacBook-Pro.local.el Add .doom.d/JoshGilsMBP2021.lan.el Add .doom.d/config.el Add .doom.d/config.org Add .doom.d/custom.el Add .doom.d/.stfolder/.keep Add .doom.d/framework.el Add .doom.d/init.el Add .doom.d/packages.el Add .doom.d/themes/doom-nano-dark-theme.el Add .doom.d/towerofpower.el

---
+++ dot_doom.d/framework.el
@@ -0,0 +1,23 @@
+;;; framework.el -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Josh
+;;
+;; Created: May 20, 2022
+;; Modified: May 20, 2022
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+
+(setq doom-font (font-spec :family "Iosevka Nerd Font Mono" :size 18))
+(setq doom-variable-pitch-font (font-spec :family "Iosevka Nerd Font" :size 18))
+
+(provide 'framework)
+;;; framework.el ends here
---
+++ dot_doom.d/towerofpower.el
@@ -0,0 +1,25 @@
+;;; towerofpower.el -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Josh
+;;
+;; Created: May 20, 2022
+;; Modified: May 20, 2022
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/josh/towerofpower
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+
+(setq doom-font (font-spec :family "Iosevka Nerd Font Mono" :size 13))
+(setq doom-variable-pitch-font (font-spec :family "Iosevka Nerd Font" :size 13))
+
+(provide 'towerofpower)
+;;; towerofpower.el ends here
