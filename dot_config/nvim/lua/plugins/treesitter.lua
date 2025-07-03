bfc8bc918f18aa0d073eb6c6d060d9c4e335a8f0
Josh <josh@joshgilliland.com>
2025-07-03 07:44:00 -0500

    Adding nvim

---
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+-- File: ~/.config/nvim/lua/plugins/treesitter.lua
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "fish",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "regex",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+  },
+}
