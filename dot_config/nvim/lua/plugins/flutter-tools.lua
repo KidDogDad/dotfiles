4cdd57a7c9e4e25e1b731f999bbe35862083424c
Josh <josh@joshgilliland.com>
2025-07-21 16:51:08 -0500

    Update .config/nvim/lua/config/options.lua Update .config/nvim/lua/plugins/flutter-tools.lua

--- dot_config/nvim/lua/plugins/flutter-tools.lua
+++ dot_config/nvim/lua/plugins/flutter-tools.lua
@@ -3,7 +3,28 @@ return {
   lazy = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
-    --"stevearc/dressing.nvim", -- optional for vim.ui.select
+    "stevearc/dressing.nvim", -- optional for vim.ui.select
   },
-  config = true,
+  config = function()
+    require("flutter-tools").setup({
+      -- LSP configuration is the most important part
+      lsp = {
+        -- This is the main settings object for the dart_language_server
+        settings = {
+          showTodos = true,
+          completeFunctionCalls = true,
+          analysisExcludedFolders = { "**/build/**" },
+          renameFilesWithClasses = "prompt", -- "always" or "never"
+          enableSnippets = true,
+        },
+      },
+      -- Other plugin settings
+      -- decorations = {
+      --   statusline = {
+      --     --  What symbols to use for the statusline component
+      --     device = "📱",
+      --   },
+      -- },
+    })
+  end,
 }
