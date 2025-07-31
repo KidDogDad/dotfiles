56283503fc2547782171b3c3e5b6d80a33fabc0b
Josh <josh@joshgilliland.com>
2025-07-31 18:52:28 -0500

    Update .doom.d/config.el Update .doom.d/config.org Update .doom.d/custom.el

--- dot_doom.d/custom.el
+++ dot_doom.d/custom.el
@@ -4,12 +4,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" default)))
+   '("8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" default))
+ '(org-agenda-files
+   '("~/Sync/roam/daily/2025-07-31.org" "/home/josh/Sync/roam/test.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil))))
  '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "variable-pitch"))))
  '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.6))))
  '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.4))))
